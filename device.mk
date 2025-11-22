28d7afdef011d8c4b919308fbeaba34d746070df
Davi0293 <159967899+Davi0293@users.noreply.github.com>
2025-11-22 05:29:03 -0300

    Fix formatting in device.mk for PRODUCT_PACKAGES
    Added the missing separators,also known as \

--- device.mk
+++ device.mk
@@ -210,7 +210,7 @@ $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
         $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/bin/$(notdir $f)))
 
 PRODUCT_PACKAGES += \
-    init.qcom.recovery.rc
+    init.qcom.recovery.rc \
     init.ets.rc \
     init.mmi.overlay.rc \
     init.recovery.xpeng.rc \
@@ -365,7 +365,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.multihal
+    android.hardware.sensors-service.multihal \
     sensors.xpeng
 
 PRODUCT_COPY_FILES += \
